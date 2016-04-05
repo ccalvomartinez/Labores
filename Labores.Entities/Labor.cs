875f41d43c3dee640348a4d3a8f4643918f2a0e0
Carolina Calvo <pececillu@gmail.com>
2016-04-05 13:33:37 +0200

    Data

---
+++ Labores.Entities/Labor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labores.Entities
+{
+    public class Labor
+    {
+        private int id;
+        public List<Material> Materiales { get; set; }
+        public System.Xml.XmlDocument Instrucciones { get; set; }
+    }
+}
--- Labores.Entities/Material.cs
+++ Labores.Entities/Material.cs
@@ -2,11 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Labores.Entities
 {
-    public class Class1
+    class Material
     {
     }
 }
